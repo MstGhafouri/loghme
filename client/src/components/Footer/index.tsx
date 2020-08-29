2b948d83fbebe2d5e08f58fdbba13f259e319a2f
Mostafa Ghafouri <mstghafouri@gmail.com>
2020-08-29 13:22:54 +0430

    Update footer copyright

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.SFC = () => {
   return (
     <footer className="footer">
-      <p className="footer__text"> &copy; تمامی حقوق متعلق به لقمه است.</p>
+      <p className="footer__text"> <small>by Mostafa Ghafouri</small> &copy; تمامی حقوق متعلق به لقمه است.</p>
     </footer>
   );
 };
